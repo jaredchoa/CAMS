c1202e5a536f8c20fb7c067947368f3baf48cc11
jdengoh <spect13317@gmail.com>
2023-11-27 23:30:25 +0800

    removed old docs

--- Camp Application and Management System/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"CAMS","l":"application"},{"m":"CAMS","l":"common.model"},{"m":"CAMS","l":"common.util"},{"m":"CAMS","l":"controller"},{"m":"CAMS","l":"controller.manager"},{"m":"CAMS","l":"fileIO"},{"m":"CAMS","l":"Handlers"},{"m":"CAMS","l":"model"}];updateSearchResults();
