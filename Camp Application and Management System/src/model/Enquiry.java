1ee7b972c1d97db5092dc2e8715c505f583a5f4b
jdengoh <spect13317@gmail.com>
2023-11-27 14:00:12 +0800

    enquiry javadog

--- Camp Application and Management System/src/model/Enquiry.java
+++ Camp Application and Management System/src/model/Enquiry.java
@@ -1,5 +1,8 @@
 package model;
 
+/**
+ * Class that stores information about enquiry
+ */
 public class Enquiry {
 	public static int ENQUIRYCOUNTER = 1;
 	
@@ -11,6 +14,13 @@ public class Enquiry {
 	
 	
 	
+	/**
+	 * @param enquiryID unique identifier for enquiry
+	 * @param Enquiry enquiry details
+	 * @param reply reply details
+	 * @param sender student that send enquiry
+	 * @param camp camp associated with enquiry
+	 */
 	public Enquiry(String enquiryID, String Enquiry, String reply, User sender, String camp) {
 		
 		this.Enquiry = Enquiry;
@@ -23,6 +33,11 @@ public class Enquiry {
 	};
 	
 	
+	/**
+	 * @param Enquiry enquiry details
+	 * @param sender student that send enquiry
+	 * @param camp camp associated with enquiry
+	 */
 	public Enquiry(String Enquiry, User sender, String camp) {
 		this.Enquiry = Enquiry;
 		this.EnquiryID = "E" + ENQUIRYCOUNTER;
@@ -33,46 +48,79 @@ public class Enquiry {
 		ENQUIRYCOUNTER++;
 	}
 
+	/**
+	 * @return enquiry
+	 */
 	public String getEnquiry() {
 		return Enquiry;
 	}
 
+	/**
+	 * @param enquiry enquiry
+	 */
 	public void setEnquiry(String enquiry) {
 		Enquiry = enquiry;
 	}
 
+	/**
+	 * @return enquiry ID
+	 */
 	public String getEnquiryID() {
 		return EnquiryID;
 	}
 
+	/**
+	 * @param enquiryID enquiry ID
+	 */
 	public void setEnquiryID(String enquiryID) {
 		EnquiryID = enquiryID;
 	}
 
+	/**
+	 * @return reply
+	 */
 	public String getReply() {
 		return Reply;
 	}
 
+	/**
+	 * @param reply reply
+	 */
 	public void setReply(String reply) {
 		Reply = reply;
 	}
 	
+	/**
+	 * @param sender sender
+	 */
 	public void setSender(User sender) {
 		Sender = sender;
 	}
 
+	/**
+	 * @return sender
+	 */
 	public User getSender() {
 		return Sender;
 	}
 	
+	/**
+	 * @param camp camp
+	 */
 	public void setCampID(String camp) {
 		CampID = camp;
 	}
 
+	/**
+	 * @return camp
+	 */
 	public String getCampID() {
 		return CampID;
 	}
 	
+	/**
+	 * print enquiry details
+	 */
 	public void printEnquiry() {
 		System.out.println(
 				"\nEnquiryID: " + this.getEnquiryID()  + "\n" +
